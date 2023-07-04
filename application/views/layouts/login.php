96fe1eca2280dcf19d6edf023b798b00aa4acf43
hamintonjair <85085152+hamintonjair@users.noreply.github.com>
2023-07-04 10:52:28 -0500

    Update login.php

--- application/views/layouts/login.php
+++ application/views/layouts/login.php
@@ -19,7 +19,7 @@
                             <div class="col-lg-5">
                                 <div class="card shadow-lg border-0 rounded-lg mt-5">
                                     
-                                    <div class="card-header"><h3 class="text-center font-weight-light my-4">Iniciar Sesión</h3></div>
+                                    <div class="card-header"><h3 class="text-center font-weight-light my-4">Iniciar Sesión - Efi-conta V1.0</h3></div>
                                     <div class="card-body">                                  
                                         <form id="frmLogin" name="frmLogin">
                                             <div class="form-group">
